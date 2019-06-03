41e64e2a88e4db07fcb618c22e919ce09394d293
ALINE IRASEMA FLORES DELON <aline@nube.unadmexico.mx>
2019-06-03 14:45:11 -0500

    Capa COMMON
    Realizado con el tutorial de Carlos Espinoza

---
+++ Inventario.COMMON/Interfaces/IRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventario.COMMON.Interfaces
+{
+    public interface IRepositorio <T> where T: Entidades.Base
+    {
+        bool Crear(T entidad);
+        bool Editar(string id, T entidadModificada);
+        bool Eliminar(T entidad);
+        List<T> Leer { get; }
+    }
+}
